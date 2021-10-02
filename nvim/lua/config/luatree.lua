8a33fd7969f2246366b223cdd9d4ff37a9afd152
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-10-02 14:49:01 +0200

    fix breaking changes

--- nvim/lua/config/luatree.lua
+++ nvim/lua/config/luatree.lua
@@ -3,6 +3,21 @@
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 local nnoremap = vim.keymap.nnoremap
 
+require('nvim-tree').setup({
+    view = {
+        mappings = {
+            list = {
+                { key = 'r', cb = tree_cb('full_rename') },
+                { key = '<C-r>', cb = tree_cb('rename') },
+                { key = 'J', cb = ":exe 'normal 10j'<CR>" },
+                { key = 'K', cb = ":exe 'normal 10k'<CR>" },
+                { key = 'dd', cb = tree_cb('remove') },
+                { key = '..', cb = tree_cb('dir_up') },
+            },
+        },
+    },
+})
+
 vim.g.nvim_tree_icons = {
     default = '',
     git = {
@@ -25,15 +40,5 @@ vim.g.nvim_tree_special_files = {
 }
 
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache', '.DS_Store' }
-vim.g.nvim_tree_git_hl = 1
 
 nnoremap({ '<leader>tt', ':NvimTreeToggle<CR>' })
-
-vim.g.nvim_tree_bindings = {
-    { key = 'r', cb = tree_cb('full_rename') },
-    { key = '<C-r>', cb = tree_cb('rename') },
-    { key = 'J', cb = ":exe 'normal 10j'<CR>" },
-    { key = 'K', cb = ":exe 'normal 10k'<CR>" },
-    { key = 'dd', cb = tree_cb('remove') },
-    { key = '<CR>', cb = tree_cb('edit') },
-}
