2271ee56f00600a6fa9408d9b8dfb4f2a9de5ccc
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-10-07 10:07:34 +0200

    rework lsp rename handler

--- nvim/lua/config/lspconfig/rename.lua
+++ nvim/lua/config/lspconfig/rename.lua
@@ -6,10 +6,20 @@ local options = {
     prompt = '> ',
 }
 
-local confirm = function()
-    local new_name = vim.trim(vim.fn.getline('.'):sub(#options.prompt + 1, -1))
-    vim.api.nvim_win_close(0, true)
-    local params = vim.lsp.util.make_position_params()
+local request = function(method, params, handler)
+    vim.validate({
+        method = { method, 's' },
+        handler = { handler, 'f', true },
+    })
+    return vim.lsp.buf_request(0, method, params, handler)
+end
+
+local confirm = function(params, new_name)
+    local close = new_name == nil
+    new_name = new_name or vim.trim(vim.fn.getline('.'):sub(#options.prompt + 1, -1))
+    if close then
+        vim.api.nvim_win_close(0, true)
+    end
     local current_name = vim.fn.expand('<cword>')
     vim.cmd('stopinsert')
     if not (new_name and #new_name > 0) or new_name == current_name then
@@ -19,26 +29,39 @@ local confirm = function()
     vim.lsp.buf_request(0, 'textDocument/rename', params)
 end
 
-M.rename = function()
-    local popup_bufnr, _ = ui.popup_create({
-        width = 30,
-        height = 1,
-        enter = true,
-        prompt = {
-            enable = true,
-            prefix = options.prompt,
-            highlight = 'LspRenamePrompt',
-        },
-    })
-    vim.api.nvim_buf_set_option(popup_bufnr, 'modifiable', true)
-    inoremap({
-        '<CR>',
-        function()
-            confirm()
-        end,
-        buffer = true,
-    })
-    vim.cmd('startinsert')
+M.rename = function(new_name)
+    local params = vim.lsp.util.make_position_params()
+    if new_name then
+        confirm(params, new_name)
+        return
+    end
+    local prepare_rename = function(err, result)
+        if err == nil and result == nil then
+            print('Nothing to rename')
+            return
+        end
+        local popup_bufnr, _ = ui.popup_create({
+            width = 30,
+            height = 1,
+            enter = true,
+            prompt = {
+                enable = true,
+                prefix = options.prompt,
+                highlight = 'LspRenamePrompt',
+            },
+        })
+        vim.api.nvim_buf_set_option(popup_bufnr, 'modifiable', true)
+        inoremap({
+            '<CR>',
+            function()
+                confirm(params)
+            end,
+            buffer = true,
+        })
+        vim.cmd('startinsert')
+    end
+
+    request('textDocument/prepareRename', params, prepare_rename)
 end
 
 return M
