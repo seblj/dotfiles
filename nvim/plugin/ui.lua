2552ba2f646fa61393886b9a352a596bfe9e7ef1
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-11-07 22:50:57 +0100

    override vim.ui.input after it getting merged into neovim

--- nvim/plugin/ui.lua
+++ nvim/plugin/ui.lua
@@ -2,10 +2,15 @@ local ui = require('seblj.utils.ui')
 local utils = require('seblj.utils')
 local augroup = utils.augroup
 local nnoremap = vim.keymap.nnoremap
+local inoremap = vim.keymap.inoremap
 
 if not vim.ui then
     return
 end
+
+local options = {
+    prefix = '> ',
+}
 
 local confirm = function(items, on_choice, key)
     local choice = tonumber(vim.fn.expand('<cword>'))
@@ -17,6 +22,7 @@ local confirm = function(items, on_choice, key)
     on_choice(items[choice], choice)
 end
 
+-- Override vim.ui.select to use popup
 vim.ui.select = function(items, opts, on_choice)
     vim.validate({
         items = { items, 'table', false },
@@ -67,4 +73,43 @@ vim.ui.select = function(items, opts, on_choice)
 
     -- stylua: ignore
     nnoremap({ '<CR>', function() confirm(items, on_choice) end, buffer = true })
+end
+
+-- Override vim.ui.input to use popup
+vim.ui.input = function(opts, on_confirm)
+    vim.validate({
+        on_confirm = { on_confirm, 'function', false },
+    })
+    opts = opts or {}
+
+    local lines = {}
+    local title = opts.prompt
+    lines = { title, string.rep(ui.border_line, 30), unpack(lines) }
+
+    local popup_bufnr, _ = ui.popup_create({
+        width = 30,
+        lines = lines,
+        height = 3,
+        enter = true,
+        prompt = {
+            enable = true,
+            prefix = options.prefix,
+            highlight = 'LspRenamePrompt',
+        },
+    })
+    vim.api.nvim_buf_set_option(popup_bufnr, 'modifiable', true)
+    vim.api.nvim_buf_add_highlight(popup_bufnr, -1, 'Title', 0, 0, #title)
+    vim.api.nvim_buf_add_highlight(popup_bufnr, -1, 'FloatBorder', 1, 0, -1)
+
+    inoremap({
+        '<CR>',
+        function()
+            local input = vim.trim(vim.fn.getline('.'):sub(#options.prefix + 1, -1))
+            vim.api.nvim_win_close(0, true)
+            on_confirm(input)
+            vim.cmd('stopinsert')
+        end,
+        buffer = true,
+    })
+    vim.cmd('startinsert')
 end
