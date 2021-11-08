9060a8391ceb2ac8d127818d07662713c88a1bf2
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-11-08 21:14:54 +0100

    removed old install script

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Sets varibale to dotfiles location
-DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
-
-# This sets variables needed for installation
-source $DOTFILES/utils/variables.sh
-source $DOTFILES/utils/colors.sh
+OS=$(uname -s)
 
 if [[ $OS == "Linux" ]]; then
     sudo apt update
     sudo apt install software-properties-common
     sudo add-apt-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
+    sudo apt install -y ansible
 else
     pip install ansible
 fi
+
+ansible-playbook -K ansible/local.yml
+sudo chsh -s $(which zsh)
 
 # Hopefully fix insecure directories if there are any
 /bin/zsh -i -c compaudit | xargs chmod g-w,o-w >/dev/null 2>&1
