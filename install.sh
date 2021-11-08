b926c89f8611681b5a8084ee6ec2e3f4d6dc1fed
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-11-08 20:08:26 +0100

    ansible stuff

--- install.sh
+++ install.sh
@@ -7,18 +7,14 @@ DOTFILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 source $DOTFILES/utils/variables.sh
 source $DOTFILES/utils/colors.sh
 
-# Installs homebrew and symlinks .macos if OS is macOS. Needs to be sourced before doing $UPDATE
-source $DOTFILES/install/mac.sh
-
-# Runs 'brew update' or 'sudo apt-get update' based on OS
-$UPDATE
-
-# Installation
-source $DOTFILES/install/common.sh
-source $DOTFILES/install/neovim.sh
-source $DOTFILES/install/zsh.sh
+if [[ $OS == "Linux" ]]; then
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+else
+    pip install ansible
+fi
 
 # Hopefully fix insecure directories if there are any
 /bin/zsh -i -c compaudit | xargs chmod g-w,o-w >/dev/null 2>&1
-
-printf "\n${BLUE}Finishing up! ${NC}\n\n"
