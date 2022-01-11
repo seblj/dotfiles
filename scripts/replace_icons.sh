86eabbf032cacc3941cd3bc08aedd3d3735ea311
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-01-11 21:12:54 +0100

    add icons and script to replace icons on macos

---
+++ scripts/replace_icons.sh
@@ -0,0 +1,5 @@
+fileicon set /Applications/iTerm.app $DOTFILES/icons/iTerm2.icns
+fileicon set /Applications/Plex.app $DOTFILES/icons/Plex.icns 
+fileicon set /Applications/kitty.app $DOTFILES/icons/kitty.icns 
+
+sudo killall Finder && sudo killall Dock
