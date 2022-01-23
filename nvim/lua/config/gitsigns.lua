bc5d9826d8668d19000cda3faa9bb23de1538bea
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-01-23 20:39:11 +0100

    use gitsigns on_attach

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -1,7 +1,5 @@
 ---------- GITSIGNS CONFIG ----------
 
-local keymap = vim.keymap.set
-
 require('gitsigns').setup({
     signs = {
         add = { hl = 'GitGutterAdd', text = '+' },
@@ -14,11 +12,19 @@ require('gitsigns').setup({
         border = 'rounded',
     },
     max_file_length = 200000,
-})
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local map = function(m, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(m, l, r, opts)
+        end
 
-keymap('n', '<leader>gm', '<cmd>Gitsigns blame_line<CR>', { desc = 'Gitsigns: Git blame current line' })
-keymap('n', '<leader>gn', '<cmd>Gitsigns next_hunk<CR>', { desc = 'Gitsigns: Go to next diff hunk' })
-keymap('n', '<leader>gp', '<cmd>Gitsigns prev_hunk<CR>', { desc = 'Gitsigns: Go to previous diff hunk' })
-keymap('n', '<leader>gd', '<cmd>Gitsigns preview_hunk<CR>', { desc = 'Gitsigns: Preview diff hunk' })
-keymap('n', '<leader>grh', '<cmd>Gitsigns reset_hunk<CR>', { desc = 'Gitsigns: Reset diff hunk over cursor' })
-keymap('n', '<leader>grb', '<cmd>Gitsigns reset_buffer<CR>', { desc = 'Gitsigns: Reset diff for entire buffer' })
+        map('n', '<leader>gm', gs.blame_line, { desc = 'Gitsigns: Git blame current line' })
+        map('n', '<leader>gn', gs.next_hunk, { desc = 'Gitsigns: Go to next diff hunk' })
+        map('n', '<leader>gp', gs.prev_hunk, { desc = 'Gitsigns: Go to previous diff hunk' })
+        map('n', '<leader>gd', gs.preview_hunk, { desc = 'Gitsigns: Preview diff hunk' })
+        map('n', '<leader>grh', gs.reset_hunk, { desc = 'Gitsigns: Reset diff hunk over cursor' })
+        map('n', '<leader>grb', gs.reset_buffer, { desc = 'Gitsigns: Reset diff for entire buffer' })
+    end,
+})
