28437b2a05e12acba31ab62a1ef829b321b14caa
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-01-25 20:36:43 +0100

    use built-in telescope extension for refactoring

--- nvim/lua/config/refactoring.lua
+++ nvim/lua/config/refactoring.lua
@@ -1,35 +1,9 @@
-local keymap = vim.keymap.set
 require('refactoring').setup()
+require('telescope').load_extension('refactoring')
 
--- Confirm function when pressing enter in telescope
-local function confirm(prompt_bufnr)
-    local content = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
-    require('telescope.actions').close(prompt_bufnr)
-    -- Let telescope finish and do stopinsert before vim.ui.input
-    -- is called, and startinsert is done there. If not, telescope
-    -- will make the popup start in normal mode
-    vim.schedule(function()
-        require('refactoring').refactor(content.value)
-    end)
-end
-
--- Telescope picker to display refactor methods
-local refactors = function()
-    local opts = require('telescope.themes').get_cursor()
-    require('telescope.pickers').new(opts, {
-        prompt_title = 'Refactors',
-        finder = require('telescope.finders').new_table({
-            results = require('refactoring').get_refactors(),
-        }),
-        sorter = require('telescope.config').values.generic_sorter(opts),
-        attach_mappings = function(_, map)
-            map('i', '<CR>', confirm)
-            map('n', '<CR>', confirm)
-            return true
-        end,
-    }):find()
-end
-
-keymap('v', '<leader>fr', function()
-    refactors()
-end, { desc = 'Refactoring: Open menu' })
+vim.keymap.set(
+    'v',
+    '<leader>fr',
+    "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+    { desc = 'Refactoring: Open menu' }
+)
