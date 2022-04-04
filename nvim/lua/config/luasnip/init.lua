3200c014d8481a61524a11bd3fbd1a22abab5255
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-04-04 11:30:03 +0200

    make luasnip work again

--- nvim/lua/config/luasnip/init.lua
+++ nvim/lua/config/luasnip/init.lua
@@ -1,4 +1,17 @@
 local ls = require('luasnip')
+local keymap = vim.keymap.set
+
+keymap({ 'i', 's' }, '<C-j>', function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end)
+
+keymap({ 'i', 's' }, '<C-k>', function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end)
 
 local snippets = {}
 
@@ -12,4 +25,6 @@ snippets.c = require('config.luasnip.c')
 snippets.vue = require('config.luasnip.vue')
 snippets.rust = require('config.luasnip.rust')
 
-ls.snippets = snippets
+for lang, snips in pairs(snippets) do
+    ls.add_snippets(lang, snips)
+end
