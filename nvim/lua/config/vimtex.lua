12bdbdbb97a99bd1ed6bf3f56cb904d136edc696
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-08-24 14:25:48 +0200

    use skim only on mac

--- nvim/lua/config/vimtex.lua
+++ nvim/lua/config/vimtex.lua
@@ -5,9 +5,14 @@ local autocmd = vim.api.nvim_create_autocmd
 
 vim.g.tex_flavor = 'latex'
 vim.g.vimtex_quickfix_mode = 0
-vim.g.vimtex_general_viewer = 'open -a Skim'
-vim.g.vimtex_view_method = 'skim'
 vim.g.vimtex_syntax_enabled = 0
+
+if vim.fn.has('mac') == 1 then
+    vim.g.vimtex_general_viewer = 'open -a Skim'
+    vim.g.vimtex_view_method = 'skim'
+else
+    vim.g.vimtex_view_method = 'zathura'
+end
 
 -- Clean latex files when quitting
 local group = augroup('VimtexConfig', {})
