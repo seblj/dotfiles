e9344c4a982aaaebbcf0ddd145845908892b81be
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-10-31 16:59:01 +0100

    stuff

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,8 +1,10 @@
 # Exports
 
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_LAUNCHD_SESSION_BUS_SOCKET"
 export DOTFILES="$HOME/dotfiles"
 export OS=$(uname -s)
 export XDG_CONFIG_HOME="$HOME/.config"
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
 export _Z_DATA="$HOME/.config/z/.z."
 export FZF_DEFAULT_OPTS='--height 40%'
