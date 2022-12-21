f7ff53ee0192ef168d70e6dbe1be48ce1c034788
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-12-21 22:18:03 +0100

    add workaround for annoying lazy and startify problem

--- nvim/after/ftplugin/lazy.lua
+++ nvim/after/ftplugin/lazy.lua
@@ -2,3 +2,9 @@ vim.opt.guicursor = vim.opt.guicursor + 'a:Cursor/lCursor'
 vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
 vim.opt_local.cursorline = false
 require('lazy.view').hover = 'gd'
+
+-- Add workaround for issue with startify
+vim.defer_fn(function()
+    vim.keymap.del('n', 'q', { buffer = true })
+    vim.keymap.set('n', 'q', ':q<CR>', { buffer = true })
+end, 150)
