4bc4dbe05d27fbe45340cb86f8cec87deae20237
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-01-10 11:02:23 +0100

    fix mouse hover

--- nvim/lua/config/lspconfig/hover.lua
+++ nvim/lua/config/lspconfig/hover.lua
@@ -1,23 +1,18 @@
 local M = {}
 local util = require('vim.lsp.util')
 
-local function make_position_param(window, offset_encoding)
-    window = window or 0
-    local buf = vim.api.nvim_win_get_buf(window)
-    local row, col
-
-    local mouse = vim.fn.getmousepos()
-    row = mouse.line
-    col = mouse.column
-
-    offset_encoding = offset_encoding or util._get_offset_encoding(buf)
-    row = row - 1
-    local line = vim.api.nvim_buf_get_lines(buf, row, row + 1, true)[1]
-    if not line then
-        return { line = 0, character = 0 }
+local function make_position_param(mouse, bufnr, offset_encoding)
+    local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+    if not clients then
+        return
     end
-    if #line < col then
-        return { line = 0, character = 0 }
+    local row = mouse.line - 1
+    local col = mouse.column
+
+    offset_encoding = offset_encoding or util._get_offset_encoding(bufnr)
+    local line = vim.api.nvim_buf_get_lines(bufnr, row, row + 1, true)[1]
+    if not line or #line < col then
+        return nil
     end
 
     col = util._str_utfindex_enc(line, col, offset_encoding)
@@ -25,14 +20,39 @@ local function make_position_param(window, offset_encoding)
     return { line = row, character = col }
 end
 
-local make_params = function(window, offset_encoding)
-    window = window or 0
-    local buf = vim.api.nvim_win_get_buf(window)
-    offset_encoding = offset_encoding or util._get_offset_encoding(buf)
+local make_params = function(mouse, bufnr, offset_encoding)
+    offset_encoding = offset_encoding or util._get_offset_encoding(bufnr)
+    local position = make_position_param(mouse, bufnr, offset_encoding)
+    if not position then
+        return nil
+    end
     return {
-        textDocument = util.make_text_document_params(buf),
-        position = make_position_param(window, offset_encoding),
+        textDocument = util.make_text_document_params(bufnr),
+        position = position,
     }
+end
+
+local function hover_handler(_, result, ctx, config)
+    config = {
+        border = CUSTOM_BORDER,
+        silent = true,
+        focusable = false,
+        relative = 'mouse',
+        focus_id = ctx.method,
+    }
+    if not (result and result.contents) then
+        if config.silent ~= true then
+            vim.notify('No information available')
+        end
+        return
+    end
+    local markdown_lines = util.convert_input_to_markdown_lines(result.contents, {})
+    markdown_lines = util.trim_empty_lines(markdown_lines)
+    if vim.tbl_isempty(markdown_lines) then
+        vim.notify('No information available')
+        return
+    end
+    return util.open_floating_preview(markdown_lines, 'markdown', config)
 end
 
 M.setup = function()
@@ -45,21 +65,16 @@ M.setup = function()
         end
         hover_timer = vim.defer_fn(function()
             hover_timer = nil
-            local params = make_params()
-            vim.lsp.buf_request(
-                0,
-                'textDocument/hover',
-                params,
-                vim.lsp.with(vim.lsp.handlers.hover, {
-                    border = CUSTOM_BORDER,
-                    silent = true,
-                    focusable = false,
-                    relative = 'mouse',
-                })
-            )
+            local mouse = vim.fn.getmousepos()
+            local bufnr = vim.api.nvim_win_get_buf(mouse.winid)
+            local params = make_params(mouse, bufnr)
+            if not params then
+                return
+            end
+            vim.lsp.buf_request(bufnr, 'textDocument/hover', params, hover_handler)
         end, 500)
         return '<MouseMove>'
-    end, { expr = true, buffer = true })
+    end, { expr = true })
 end
 
 return M
