30ea082f4d5697b57c13607765b25a33256bc4f3
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-02-03 09:22:27 +0100

    remove queries that mess with macro injection in rust

--- nvim/after/queries/rust/injections.scm
+++ nvim/after/queries/rust/injections.scm
@@ -2,31 +2,6 @@
 
 (macro_invocation
     (scoped_identifier
-        path: (identifier) @_path (#not-eq? @_path "sqlx")
-        name: (identifier) @_name (#not-match? @_name "query")
-    )
-    (token_tree) @rust
-)
-
-(macro_invocation
-    macro: (identifier) @_macro (#not-any-of? @_macro "fetch_optional" "fetch_all" "insert" "execute")
-    (token_tree) @rust
-)
-
-(macro_invocation
-    (token_tree
-      (identifier) @rust
-    )
-)
-
-(macro_invocation
-    (token_tree
-      (token_tree) @rust
-    )
-)
-
-(macro_invocation
-    (scoped_identifier
         path: (identifier) @_path (#eq? @_path "sqlx")
         name: (identifier) @_name (#match? @_name "query")
     )
@@ -53,43 +28,3 @@
     )
     (#offset! @sql 0 3 0 -2)
 )
-
-;
-; Default values from ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/queries/rust/injections.scm
-;
-
-; Replaced with the three `macro_invocation`'s at top of file
-; (macro_invocation
-;   (token_tree) @rust)
-
-(macro_definition
-  (macro_rule
-    left: (token_tree_pattern) @rust
-    right: (token_tree) @rust))
-
-[
-  (line_comment)
-  (block_comment)
-] @comment
-
-(
-  (macro_invocation
-    macro: ((identifier) @_html_def)
-    (token_tree) @html)
-
-    (#eq? @_html_def "html")
-)
-
-(call_expression
-  function: (scoped_identifier
-    path: (identifier) @_regex (#eq? @_regex "Regex")
-    name: (identifier) @_new (#eq? @_new "new"))
-  arguments: (arguments
-    (raw_string_literal) @regex))
-
-(call_expression
-  function: (scoped_identifier
-    path: (scoped_identifier (identifier) @_regex (#eq? @_regex "Regex").)
-    name: (identifier) @_new (#eq? @_new "new"))
-  arguments: (arguments
-    (raw_string_literal) @regex))
