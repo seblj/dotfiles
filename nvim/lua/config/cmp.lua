5478756a920d944c97c924a88af77096db98a2ba
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-02-24 08:10:08 +0100

    make lua_ls happy

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -30,7 +30,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping.complete({}),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
     }),
 
