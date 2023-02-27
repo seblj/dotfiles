366420471cc27423acfaf535e7ab1366e0263cca
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-02-27 09:11:36 +0100

    fix emojify link

--- install/packages.sh
+++ install/packages.sh
@@ -51,7 +51,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y >/dev/nu
 
 # Emojify
 printf "\n${BLUE}Installing emojify ${NC}\n\n"
-sh -c "curl -s https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o /usr/local/bin/emojify && chmod +x /usr/local/bin/emojify"
+sudo sh -c "curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o /usr/local/bin/emojify && chmod +x /usr/local/bin/emojify"
 
 # Cargo is not exported into path right after installing, so just use fullpath
 # for it
