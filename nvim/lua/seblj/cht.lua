0edae7d0ad470361011f90059e1284baa2d2acb4
Sebastian Lyng Johansen <seblyng98@gmail.com>
2023-03-03 23:35:16 +0100

    more telescope changes

--- nvim/lua/seblj/cht.lua
+++ nvim/lua/seblj/cht.lua
@@ -1,6 +1,4 @@
 local M = {}
-local utils = require("seblj.utils")
-local run_term = utils.term
 
 local languages = {
     "golang",
@@ -16,47 +14,31 @@ local core_utils = {
     "find",
 }
 
-local telescope_selected = {}
-
 local total = vim.list_extend(languages, core_utils)
 
-local function run_cht(query)
-    local selected = telescope_selected[1]
-    local input = vim.split(query, " ")
-
-    if vim.tbl_contains(languages, selected) then
-        query = table.concat(input, "+")
-    else
-        query = table.concat(input, "~")
-    end
-
-    run_term({
-        direction = "tabnew",
-        focus = true,
-        stopinsert = true,
-        cmd = string.format("curl cht.sh/%s/%s", selected, query),
-    })
-end
-
-local function confirm(prompt_bufnr)
-    local content = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
-    require("telescope.actions").close(prompt_bufnr)
-    table.insert(telescope_selected, content.value)
-    vim.ui.input({ prompt = "Query: " }, run_cht)
-end
-
 function M.telescope_cht()
-    local opts = require("telescope.themes").get_cursor()
     require("telescope.pickers")
-        .new(opts, {
+        .new(require("telescope.themes").get_cursor(), {
             prompt_title = "Cheat sheet",
             finder = require("telescope.finders").new_table({
-                results = vim.tbl_values(total),
+                results = total,
             }),
-            sorter = require("telescope.config").values.generic_sorter(opts),
             attach_mappings = function(_, map)
-                map("i", "<CR>", confirm)
-                map("n", "<CR>", confirm)
+                map({ "i", "n" }, "<CR>", function(bufnr)
+                    local content = require("telescope.actions.state").get_selected_entry()
+                    require("telescope.actions").close(bufnr)
+                    vim.ui.input({ prompt = "Query: " }, function(query)
+                        local input = vim.split(query, " ")
+                        query = table.concat(input, vim.tbl_contains(languages, content.value) and "+" or "~")
+
+                        require("seblj.utils").run_term({
+                            direction = "tabnew",
+                            focus = true,
+                            stopinsert = true,
+                            cmd = string.format("curl cht.sh/%s/%s", content.value, query),
+                        })
+                    end)
+                end)
                 return true
             end,
         })
