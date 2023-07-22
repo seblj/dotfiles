c5d5d2333c5cf22edc56d33ae22757984970322e
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2023-07-22 14:54:07 +0200

    updates

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -15,6 +15,17 @@ diff_commit() {
     fi
 }
 
+# TODO: Maybe add an override flag
+git() {
+    # If we are inside a git repo at work, and does not use -h or --help with
+    # push, and the branch is either master or d2d, then refuse the push
+    if [[ -d .git && "$1" == "push" && "$@" != *"--help"* && "$@" != *"-h"* && $PWD == */work* ]]; then
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        [[ $branch == *master* || $branch == *d2d* ]] && echo "Do not push to master or d2d" && exit 1
+    fi
+    command git "$@"
+}
+
 fzf_gitmoji() {
     local cmd="cat $HOME/dotfiles/zsh/gitmoji.json | jq -r '.[] | \"\(.emoji) \(.code) \(.description)\"'"
     local result="$(eval "$cmd" | fzf | xargs echo | grep -Eo ':\w+:')"
@@ -48,8 +59,8 @@ zip() {
     zipname=$1
     if [ $2 ] ; then
         case $2 in
-            *.*)            zipname=$2;;
-            *)              zipname=$2.zip;;
+            *.*)            zipname=$2 ;;
+            *)              zipname=$2.zip ;;
         esac
     fi
     command zip -r $zipname $1
@@ -59,18 +70,18 @@ zip() {
 extract () {
     if [ -f $1 ] ; then
         case $1 in
-            *.tar.bz2)   tar xjf $1;;
-            *.tar.gz)    tar xzf $1;;
-            *.bz2)       bunzip2 $1;;
-            *.rar)       rar x $1;;
-            *.gz)        gunzip $1;;
-            *.tar)       tar xf $1;;
-            *.tbz2)      tar xjf $1;;
-            *.tgz)       tar xzf $1;;
-            *.zip)       unzip $1;;
-            *.Z)         uncompress $1;;
-            *.7z)        7z x $1;;
-            *)           echo "'$1' cannot be extracted via extract()";;
+            *.tar.bz2)   tar xjf $1 ;;
+            *.tar.gz)    tar xzf $1 ;;
+            *.bz2)       bunzip2 $1 ;;
+            *.rar)       rar x $1 ;;
+            *.gz)        gunzip $1 ;;
+            *.tar)       tar xf $1 ;;
+            *.tbz2)      tar xjf $1 ;;
+            *.tgz)       tar xzf $1 ;;
+            *.zip)       unzip $1 ;;
+            *.Z)         uncompress $1 ;;
+            *.7z)        7z x $1 ;;
+            *)           echo "'$1' cannot be extracted via extract()" ;;
         esac
     else
         echo "'$1' is not a valid file"
@@ -88,7 +99,7 @@ share() {
 
     fswatch -r0 -Ie $PWD/4913 --event Created --event Updated --event Removed -0 $PWD | while read -d "" event; do
 
-    rsync -avz -q -e "ssh" $PWD $1 &>/dev/null
+        rsync -avz -q -e "ssh" $PWD $1 &>/dev/null
 
     done &
 }
@@ -98,11 +109,11 @@ install_neovim() {
     cd ~/Applications
 
     case "$OS" in
-        Linux*) NVIM_DIR="nvim-linux64";;
-        Darwin*) NVIM_DIR="nvim-macos";;
-        *) 
+        Linux*) NVIM_DIR="nvim-linux64" ;;
+        Darwin*) NVIM_DIR="nvim-macos" ;;
+        *)
             echo "$OS not supported in function install_neovim"
-            return;;
+            return ;;
     esac
 
     curl -LO https://github.com/neovim/neovim/releases/download/nightly/$NVIM_DIR.tar.gz
@@ -110,7 +121,7 @@ install_neovim() {
     rm $NVIM_DIR.tar.gz
     rm -rf nvim-nightly
     case "$OS" in
-        Darwin*) NVIM_DIR="nvim-osx64";;
+        Darwin*) NVIM_DIR="nvim-osx64" ;;
     esac
     mv ~/Applications/$NVIM_DIR ~/Applications/nvim-nightly
 
@@ -121,7 +132,7 @@ install_packages(){
     p=""
     arr=("$@")
     for package in "${arr[@]}"; do
-            p+="$package "
+        p+="$package "
     done
     if [[ $p != "" ]]; then
         printf "\n${BLUE}Installing $p ${NC}\n\n"
