fc7120b42d95a9cc9e2e6e51da1590a606469548
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2023-07-26 22:24:37 +0200

    add vscode init.lua and remove init.vim

---
+++ vscode/init.lua
@@ -0,0 +1,47 @@
+---------- OPTIONS ----------
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.clipboard = "unnamedplus"
+
+---------- KEYMAPS ----------
+
+vim.g.mapleader = " "
+
+vim.keymap.set({ "n", "v" }, "J", "10j")
+vim.keymap.set({ "n", "v" }, "K", "10k")
+
+vim.keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Move to beginning of line" })
+vim.keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Move to end of line" })
+
+vim.keymap.set("n", "j", 'v:count ? "j" : "gj"', { expr = true, desc = "gj" })
+vim.keymap.set("n", "k", 'v:count ? "k" : "gk"', { expr = true, desc = "gk" })
+
+vim.keymap.set("", "<leader>j", "J", { desc = "Join [count] lines" })
+
+vim.keymap.set("x", "<", "<gv", { desc = "keep visual mode on dedent" })
+vim.keymap.set("x", ">", ">gv", { desc = "keep visual mode on indent" })
+
+vim.keymap.set({ "n", "x", "i" }, "√", "<A-j>", { remap = true, desc = "Fix <A-j> mapping on mac" })
+vim.keymap.set({ "n", "x", "i" }, "ª", "<A-k>", { remap = true, desc = "Fix <A-k> mapping on mac" })
+
+vim.keymap.set({ "n", "x", "i" }, "∆", "<A-j>", { remap = true, desc = "Fix <A-j> mapping on mac" })
+vim.keymap.set({ "n", "x", "i" }, "˚", "<A-k>", { remap = true, desc = "Fix <A-k> mapping on mac" })
+
+vim.keymap.set("n", "<leader>x", ":source ~/dotfiles/vscode/init.lua<CR>")
+vim.keymap.set("n", "gcc", "<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>")
+vim.keymap.set({ "x", "o" }, "gc", "<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>")
+
+vim.keymap.set({ "n", "i", "x" }, "<A-j>", "<Cmd>call VSCodeNotify('editor.action.moveLinesDownAction')<CR>")
+vim.keymap.set({ "n", "i", "x" }, "<A-k>", "<Cmd>call VSCodeNotify('editor.action.moveLinesUpAction')<CR>")
+
+---------- AUTOCMDS ----------
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+	end,
+	desc = "Highlight on yank",
+})
