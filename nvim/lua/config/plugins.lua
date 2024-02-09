de7477b5e30b5e3bd45116550acb567efea0b636
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-02-09 22:56:59 +0100

    defer copilot to bufreadpre and bufnewfile

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -6,6 +6,7 @@ return {
 
     {
         "github/copilot.vim",
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             vim.g.copilot_filetypes = { ["*"] = false }
         end,
