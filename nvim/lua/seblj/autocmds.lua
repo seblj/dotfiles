5c1388fc64e2cb52e30d69fcb86ebb64a1cecf1c
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-05-23 22:46:51 +0200

    remove some newly added default options

--- nvim/lua/seblj/autocmds.lua
+++ nvim/lua/seblj/autocmds.lua
@@ -46,26 +46,13 @@ set_ft_option("*", function()
     vim.opt.formatoptions = vim.opt.formatoptions - "o"
 end)
 
-set_ft_option("graphql", function()
-    vim.opt_local.commentstring = "# %s"
-end)
-
-set_ft_option("typst", function()
-    vim.opt_local.commentstring = "// %s"
-end)
-
 set_ft_option({ "c", "cs" }, function()
     vim.opt_local.commentstring = "// %s"
-end)
-
-set_ft_option({ "sql" }, function()
-    vim.opt_local.commentstring = "-- %s"
 end)
 
 set_ft_option("vue", function()
     vim.opt_local.commentstring = "<!-- %s -->"
     vim.opt.formatoptions:remove("r")
-    vim.cmd.compiler("tsc")
 end)
 
 set_ft_option("help", function()
