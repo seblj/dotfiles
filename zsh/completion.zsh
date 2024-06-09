cf6fd21c77fdeb47474dc6da417255a0280e2230
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-06-09 16:08:25 +0200

    remove unused completion options

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,33 +9,18 @@ setopt auto_menu         # show completion menu on successive tab press
 setopt complete_in_word
 setopt always_to_end
 
-# should this be in keybindings?
-bindkey -M menuselect '^o' accept-and-infer-next-history
 zstyle ':completion:*:*:*:*:*' menu select
 
-# case insensitive (all), partial-word and substring completion
-if [[ "$CASE_SENSITIVE" = true ]]; then
-    zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-else
-    if [[ "$HYPHEN_INSENSITIVE" = true ]]; then
-        zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
-    else
-        zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|=*' 'l:|=* r:|=*'
-    fi
-fi
-unset CASE_SENSITIVE HYPHEN_INSENSITIVE
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|=*' 'l:|=* r:|=*'
 
 # Complete . and .. special directories
 zstyle ':completion:*' special-dirs true
 
-zstyle ':completion:*' list-colors ''
+# Color the output of `kill` command
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
-if [[ "$OSTYPE" = solaris* ]]; then
-    zstyle ':completion:*:*:*:*:processes' command "ps -u $USERNAME -o pid,user,comm"
-else
-    zstyle ':completion:*:*:*:*:processes' command "ps -u $USERNAME -o pid,user,comm -w -w"
-fi
+# Show more output when completing `ps`
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USERNAME -o pid,user,comm -w -w"
 
 # disable named-directories autocompletion
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
@@ -57,22 +42,6 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
 
 # ... unless we really want to.
 zstyle '*' single-ignored show
-
-if [[ ${COMPLETION_WAITING_DOTS:-false} != false ]]; then
-    expand-or-complete-with-dots() {
-        # use $COMPLETION_WAITING_DOTS either as toggle or as the sequence to show
-        [[ $COMPLETION_WAITING_DOTS = true ]] && COMPLETION_WAITING_DOTS="%F{red}…%f"
-        # turn off line wrapping and print prompt-expanded "dot" sequence
-        printf '\e[?7l%s\e[?7h' "${(%)COMPLETION_WAITING_DOTS}"
-        zle expand-or-complete
-        zle redisplay
-    }
-    zle -N expand-or-complete-with-dots
-    # Set the function as the default tab completion widget
-    bindkey -M emacs "^I" expand-or-complete-with-dots
-    bindkey -M viins "^I" expand-or-complete-with-dots
-    bindkey -M vicmd "^I" expand-or-complete-with-dots
-fi
 
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
